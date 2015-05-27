ba087ec13a93e4ff10a473edbd6654c07ca45964
valbub <lera-bubnova@yandex.ru>
2015-05-27 21:02:38 +0300

    Exception testing

--- Testing.py
+++ Testing.py
@@ -9,11 +9,21 @@ class TestRadixMethods(unittest.TestCase):
 
     def test_from_base_10(self):
         self.assertEqual(from_base_10('1465324', 15), '1de284')
-        with self.assertRaises(Exception):
-            from_base_10('a453', 12)
 
     def test_new_radix(self):
         self.assertEqual(new_radix('51330od', 26, 36), 'privet')
+
+    def test_from_base_10_exception(self):
+        with self.assertRaises(Exception):
+            from_base_10('a453', 12)
+
+    def test_to_base_10_exception(self):
+        with self.assertRaises(Exception):
+            to_base_10('a453', 9)
+
+    def test_new_radix_exception(self):
+        with self.assertRaises(Exception):
+            new_radix('9453', 9, 5)
 
 if __name__ == '__main__':
     unittest.main()
