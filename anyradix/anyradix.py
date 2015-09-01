b4a3a2fe9306702e2a58b06b89aea13a52c077d4
valbub <lera-bubnova@yandex.ru>
2015-09-01 22:34:34 +0300

    Changed for publishing

--- anyradix/anyradix.py
+++ anyradix/anyradix.py
@@ -1,5 +1,3 @@
-__author__ = 'Valeria'
-
 symbol = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 def to_base_10(number, old_base):
