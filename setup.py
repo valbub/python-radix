411aae16bb91ebab2fa9145d692577b920c6c8a9
valbub <lera-bubnova@yandex.ru>
2015-09-01 22:35:14 +0300

    Changed for publishing

--- setup.py
+++ setup.py
@@ -22,6 +22,8 @@ classifiers=[
     'License :: OSI Approved :: MIT License',
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
 ],
 keywords='radix numbers',
 packages=['anyradix'],
